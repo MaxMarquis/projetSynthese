7d72abec84fa6df935f65d86e96532fead5f76ee
Max Marquis <maxim.marquis@gmail.com>
2022-03-09 21:29:09 -0500

    Ajout fonction get/add/edit/delete

--- src/app/services/entreprise.service.ts
+++ src/app/services/entreprise.service.ts
@@ -1,18 +1,44 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Entreprise } from '../interfaces/entreprise';
 import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class EntrepriseService {
-  entrepriseUrl = 'https://projet-synthese-api.herokuapp.com/api/2096428/enterprise';
+  entrepriseUrl = 'https://projet-synthese-api.herokuapp.com/api/2096428/enterprise/';
 
   constructor(
     private http: HttpClient) { }
 
-  getEntreprise(): Observable<Entreprise[]> {
+  // Fonction qui va récupérer la liste DES entreprises
+  getEntreprises(): Observable<Entreprise[]> {
     return this.http.get<Entreprise[]>(this.entrepriseUrl);
   }
+
+  // Fonction qui va révupérer UNE entreprise en fonction de l'id
+  getEntreprise(__id: String): Observable<Entreprise> {
+    return this.http.get<Entreprise>(this.entrepriseUrl + __id,);
+  }
+
+  // Fonction qui ajoute une entreprise
+  addEntreprise(entreprise: Entreprise): Observable<Entreprise> {
+    return this.http.post<Entreprise>(this.entrepriseUrl, entreprise, httpOptions);
+  }
+
+  // Fonction qui modifie une entreprise en fonction de l'id
+  editEntreprise(enterprise: Entreprise): Observable<Entreprise> {
+    return this.http.put<Entreprise>(this.entrepriseUrl + enterprise.__id, enterprise, httpOptions);
+  }
+
+  // Fonction qui supprime une entreprise en fonction de l'id
+  deleteEntreprise(__id: String): Observable<Entreprise> {
+    return this.http.delete<Entreprise>(this.entrepriseUrl + __id);
+  }
+
 }

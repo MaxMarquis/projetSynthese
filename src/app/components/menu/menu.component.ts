b9478ca91196f457fd4522038373f32e677e54b9
Max Laptop <maxim.marquis@gmail.com>
2022-03-12 21:19:51 -0500

    Suppression de code test

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,22 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Entreprise } from 'src/app/interfaces/entreprise';
-import { EntrepriseService } from 'src/app/services/entreprise.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.sass']
+  styleUrls: ['./menu.component.sass'],
 })
 export class MenuComponent implements OnInit {
-  entreprises: Entreprise[] = []
+  constructor() {}
 
-  constructor(private entrepriseService: EntrepriseService) { }
-
-  ngOnInit(): void {
-    this.getEntreprise()
-  }
-
-  getEntreprise(): void {
-    this.entrepriseService.getEntreprise().subscribe((res) => (this.entreprises = res))
-  }
+  ngOnInit(): void {}
 }

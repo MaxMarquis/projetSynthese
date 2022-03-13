bd2734d9b7ba64eaa39fe1db9afbb88adbd49432
Max Laptop <maxim.marquis@gmail.com>
2022-03-12 21:20:43 -0500

    Ajout de la page Candidats, boucle la liste des candidats et appel le composant candidat

--- src/app/views/candidats/candidats.component.ts
+++ src/app/views/candidats/candidats.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Candidats } from 'src/app/interfaces/candidats';
+import { CandidatsService } from 'src/app/services/candidats.service';
 
 @Component({
   selector: 'app-candidats',
   templateUrl: './candidats.component.html',
-  styleUrls: ['./candidats.component.sass']
+  styleUrls: ['./candidats.component.sass'],
 })
 export class CandidatsComponent implements OnInit {
+  candidats: Candidats[] = [];
 
-  constructor() { }
+  constructor(private candidatService: CandidatsService) {}
 
   ngOnInit(): void {
+    this.getCandidats();
   }
 
+  getCandidats(): void {
+    this.candidatService
+      .getCandidats()
+      .subscribe((res) => (this.candidats = res));
+  }
+
+  addCandidat() {
+    alert(
+      'Doit afficher un modal ou ouvrir une page pour ajouter un candidat.'
+    );
+  }
 }

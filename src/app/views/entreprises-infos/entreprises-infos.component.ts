e302a0c0f1f9f4b6755af3f3f8f97ad005037d69
Max Marquis <maxim.marquis@gmail.com>
2022-03-27 13:04:15 -0400

    affichage de l'entreprise selecitonnée

--- src/app/views/entreprises-infos/entreprises-infos.component.ts
+++ src/app/views/entreprises-infos/entreprises-infos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Entreprise } from 'src/app/interfaces/entreprise';
+import { EntrepriseService } from 'src/app/services/entreprise.service';
 
 @Component({
   selector: 'app-entreprises-infos',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./entreprises-infos.component.sass']
 })
 export class EntreprisesInfosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  entreprise: Entreprise = {
+    _id: "123",
+    name: "",
+    description: "",
+    imageUrl: "",
+    contactName: "",
+    contactEmail: "",
+    contactPhone: "",
+    address: "",
+    city: "",
+    province: "",
+    postalCode: "",
+    published: false,
   }
 
+  constructor(private activeRoute: ActivatedRoute, private entreprisesService: EntrepriseService) { }
+
+  ngOnInit(): void {
+    const entrepriseID = this.activeRoute.snapshot.paramMap.get("id") as string;
+    console.log("Id Recu: " + entrepriseID)
+    this.getEntreprise(entrepriseID)
+  }
+
+  getEntreprise(id: string): void {
+    this.entreprisesService.getEntreprise(id).subscribe((res) => this.entreprise = res)
+  }
 }

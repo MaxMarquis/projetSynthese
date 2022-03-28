55bc8d6e058cfdd3d34095216164978ecf534019
Danny Beaunoyer <74875186+dbeaunoyer@users.noreply.github.com>
2022-03-28 15:27:48 -0400

    modif_FontAwesome

--- src/app/services/dist/auth-guard.service.js
+++ src/app/services/dist/auth-guard.service.js
@@ -10,6 +10,8 @@ exports.AuthGuardService = void 0;
 var core_1 = require("@angular/core");
 // https://www.geeksforgeeks.org/auth-guards-in-angular-9-10-11/#:~:text=AuthGuard%20is%20used%20to%20protect,canActivate%20is%20like%20a%20constructor.//
 // https://angular.io/api/router/CanActivate // 
+// nterface that a class can implement to be a guard deciding if a route can be activated. If all guards return true, navigation continues. |
+// If any guard returns false, navigation is cancelled. If any guard returns a UrlTree, the current navigation is cancelled and a new navigation begins to the UrlTree returned from the guard.//
 var AuthGuardService = /** @class */ (function () {
     function AuthGuardService(router, userService) {
         this.router = router;

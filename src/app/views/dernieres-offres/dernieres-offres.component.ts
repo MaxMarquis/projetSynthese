9cd77e78734941a56332cde5d82acef2a8656905
Danny Beaunoyer <74875186+dbeaunoyer@users.noreply.github.com>
2022-03-28 08:14:48 -0400

    Ajout_Dernieres_Offre-actif-inactif

--- src/app/views/dernieres-offres/dernieres-offres.component.ts
+++ src/app/views/dernieres-offres/dernieres-offres.component.ts
@@ -2,52 +2,67 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Offres_stages } from 'src/app/interfaces/offres_stages';
 import { OffresStagesService } from 'src/app/services/offres-stages.service';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-dernieres-offres',
   templateUrl: './dernieres-offres.component.html',
-  styleUrls: ['./dernieres-offres.component.sass']
+  styleUrls: ['./dernieres-offres.component.sass'],
 })
 export class DernieresOffresComponent implements OnInit {
-
   offresStages: Offres_stages[] = [];
 
-
-
-  constructor(private OffresStagesService: OffresStagesService,
-    private router: Router,public modalService: NgbModal) { }
+  constructor(
+    private OffresStagesService: OffresStagesService,
+    private router: Router,
+    public modalService: NgbModal
+  ) {}
 
   ngOnInit(): void {
     this.getOffresstages();
   }
 
-    /// Function get requetes_stages
+  /// Function get requetes_stages
   getOffresstages(): void {
-    this.OffresStagesService
-      .getOffresStages()
-      .subscribe((res: Offres_stages[]) => (this.offresStages = res));
+    this.OffresStagesService.getOffresStages().subscribe(
+      (res: Offres_stages[]) => (this.offresStages = res)
+    );
   }
 
   /// Function Delete requetes_stages
   onDelete(Offresstages: Offres_stages): void {
-    this.OffresStagesService.deleteOffreStage(Offresstages._id)
-      .subscribe(_result => this.offresStages = this.offresStages.filter(p => p !== Offresstages));
-
-
+    this.OffresStagesService.deleteOffreStage(Offresstages._id).subscribe(
+      (_result) =>
+        (this.offresStages = this.offresStages.filter(
+          (p) => p !== Offresstages
+        ))
+    );
+  }
+  
+  offerStatus(offresstage: Offres_stages, active: Boolean) {
+    this.OffresStagesService.editOffreStage({
+      ...offresstage,
+      active,
+    }).subscribe((_result) => {
+      offresstage.active = active;
+    });
   }
 
   editOffresstages() {
-    this.router.navigateByUrl('/offres-de-stages/edit')
+    this.router.navigateByUrl('/offres-de-stages/edit');
   }
 
-    /// Modal suppression
-  open(content:any, offresStage:Offres_stages) {
-    this.modalService.open(content, {ariaLabelledBy: 'titremodal'}).result.then((result) => {
-      if(result === 'Delete') {
-        this.onDelete(offresStage)
-      }
-    }, (reason) => {
-    });
+  /// Modal suppression
+  open(content: any, offresStage: Offres_stages) {
+    this.modalService
+      .open(content, { ariaLabelledBy: 'titremodal' })
+      .result.then(
+        (result) => {
+          if (result === 'Delete') {
+            this.onDelete(offresStage);
+          }
+        },
+        (reason) => {}
+      );
   }
 }
--- src/app/views/dernieres-offres/dist/dernieres-offres.component.js
+++ src/app/views/dernieres-offres/dist/dernieres-offres.component.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -21,15 +32,19 @@ var DernieresOffresComponent = /** @class */ (function () {
     /// Function get requetes_stages
     DernieresOffresComponent.prototype.getOffresstages = function () {
         var _this = this;
-        this.OffresStagesService
-            .getOffresStages()
-            .subscribe(function (res) { return (_this.offresStages = res); });
+        this.OffresStagesService.getOffresStages().subscribe(function (res) { return (_this.offresStages = res); });
     };
     /// Function Delete requetes_stages
     DernieresOffresComponent.prototype.onDelete = function (Offresstages) {
         var _this = this;
-        this.OffresStagesService.deleteOffreStage(Offresstages._id)
-            .subscribe(function (_result) { return _this.offresStages = _this.offresStages.filter(function (p) { return p !== Offresstages; }); });
+        this.OffresStagesService.deleteOffreStage(Offresstages._id).subscribe(function (_result) {
+            return (_this.offresStages = _this.offresStages.filter(function (p) { return p !== Offresstages; }));
+        });
+    };
+    DernieresOffresComponent.prototype.offerStatus = function (offresstage, active) {
+        this.OffresStagesService.editOffreStage(__assign(__assign({}, offresstage), { active: active })).subscribe(function (_result) {
+            offresstage.active = active;
+        });
     };
     DernieresOffresComponent.prototype.editOffresstages = function () {
         this.router.navigateByUrl('/offres-de-stages/edit');
@@ -37,12 +52,13 @@ var DernieresOffresComponent = /** @class */ (function () {
     /// Modal suppression
     DernieresOffresComponent.prototype.open = function (content, offresStage) {
         var _this = this;
-        this.modalService.open(content, { ariaLabelledBy: 'titremodal' }).result.then(function (result) {
+        this.modalService
+            .open(content, { ariaLabelledBy: 'titremodal' })
+            .result.then(function (result) {
             if (result === 'Delete') {
                 _this.onDelete(offresStage);
             }
-        }, function (reason) {
-        });
+        }, function (reason) { });
     };
     DernieresOffresComponent = __decorate([
         core_1.Component({

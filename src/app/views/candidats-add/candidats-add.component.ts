4eee5ac90de623014cff62c9fc440d49eaede6a3
Max Marquis <maxim.marquis@gmail.com>
2022-03-31 18:44:02 -0400

    Ajout du composant candidat add

--- src/app/views/candidats-add/candidats-add.component.ts
+++ src/app/views/candidats-add/candidats-add.component.ts
@@ -1,6 +1,7 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Candidats } from 'src/app/interfaces/candidats';
 
 @Component({
   selector: 'app-candidats-add',
@@ -8,6 +9,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./candidats-add.component.sass']
 })
 export class CandidatsAddComponent implements OnInit {
+  candidat: Candidats = {
+    _id: '',
+    name: '',
+    description: '',
+    jobTitle: '',
+    email: '',
+    phone: '',
+    address: '',
+    city: '',
+    province: '',
+    postalCode: '',
+    published: false,
+  };
 
   constructor(
     private location: Location
